a5db83ac87025373484ee125011093be1d9bd753
Brian McFeeley <bmcfeeley@integralads.com>
2017-07-18 13:37:42 -0400

    AE-5853. Fixing up tests for more rapid iteration by using local vault instead of virtualbox / docker-machine

--- src/test-integration/java/com/bettercloud/vault/api/SysTests.java
+++ src/test-integration/java/com/bettercloud/vault/api/SysTests.java
@@ -1,12 +1,11 @@
 package com.bettercloud.vault.api;
 
 import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
 import com.bettercloud.vault.VaultException;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import com.bettercloud.vault.json.JsonObject;
+import com.bettercloud.vault.response.SysResponse;
 import org.junit.Test;
-
-import java.io.IOException;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -14,13 +13,23 @@ import static junit.framework.TestCase.assertEquals;
  * Integration tests for the sys Vault API operations.
  */
 public class SysTests {
-    @ClassRule
-    public static final VaultContainer container = new VaultContainer();
 
-    @BeforeClass
-    public static void setupClass() throws IOException, InterruptedException {
-        container.initAndUnsealVault();
+    private static final String LOCAL_SERVER_ADDRESS = "http://127.0.0.1";
+    private static final String DEV_SERVER_ROOT_TOKEN = "813d2212-1054-152e-57d1-cf6ff46f4e81";
+
+    private static final VaultConfig testVaultConfig(int port) {
+        try {
+            return new VaultConfig()
+                    .address(LOCAL_SERVER_ADDRESS + ':' + port)
+                    .token(DEV_SERVER_ROOT_TOKEN)
+                    .build();
+        } catch (VaultException vEx) {
+            throw new RuntimeException(vEx);
+        }
     }
+
+    public static final Vault vault = new Vault(testVaultConfig(8200))
+            .withRetries(10, 100);
 
     /**
      * Verify we can get the root policy, which always exists.
@@ -30,10 +39,34 @@ public class SysTests {
     @Test
     public void testGetRootPolicy() throws VaultException {
         final String policy = "root";
-        final Vault vault = container.getRootVault();
 
-        final String policyName = vault.sys().policy("root").getName();
+        final String policyName = vault.sys().getPolicy("root").getName();
 
         assertEquals(policy, policyName);
+    }
+
+    /**
+     * Verify we can post a new policy.
+     * <p>
+     * Policies are written into configuration files matching the following form:
+     * <p>
+     * path "secret/foo" {
+     * capabilities = ["read"]
+     * }
+     *
+     * @throws VaultException
+     */
+    @Test
+    public void testWriteNewPolicy() throws VaultException {
+        final String policyName = "foo-policy";
+        final JsonObject policy = new JsonObject().add("path",
+                new JsonObject().add("secret/foo/*",
+                        new JsonObject().add("policy", "sudo")));
+
+        vault.sys().createPolicy(policyName, policy);
+        final SysResponse getResponse = vault.sys().getPolicy(policyName);
+
+        assertEquals(policyName, getResponse.getName());
+        assertEquals(policy, getResponse.getRules());
     }
 }
